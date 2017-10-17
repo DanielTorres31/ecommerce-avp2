912fa056482e4e4c2025c494cbbb437125531bab
DanielTorres <danieltorres31@outlook.com>
2017-10-17 23:08:12 +0000

    v3

--- app/js/controller.js
+++ app/js/controller.js
@@ -1,13 +1,26 @@
-angular.module('ecommerce.controllers', [])
-    .controller('BemVindoController', function($scope, $http){
-       $scope.init = function() {
-           $scope.listaCategorias = {}
-           
-           $http({
-               method: 'GET',
-               url: '/api/categoria/consultar.php?retornar_imagem=sim'
-           }).then(function(response) {
-               $scope.listaCategorias = response.data;
-           });
-       } 
+angular.module('ecommerce', ['ngRoute'])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: '/app/pages/bemvindo.html',
+                controller: 'BemVindoController'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    })
+    
+    .controller('BemVindoController', function($scope, $http) {
+        $scope.init = function() {
+            $scope.listaCategorias = {}
+
+            $http({
+                method: 'GET',
+                url: '/api/categoria/consultar.php?retornarImagem=sim'
+            }).then(function(response) {
+                $scope.listaCategorias = response.data;
+            });
+        }
     });
+
+    
