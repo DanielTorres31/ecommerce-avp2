8ddc9194e506f0cd5893194019cf476fc743f324
DanielTorres <danieltorres31@outlook.com>
2017-10-17 22:25:25 +0000

    v1

---
+++ app/js/controller.js
@@ -0,0 +1,13 @@
+angular.module('ecommerce.controllers', [])
+    .controller('BemVindoController', function($scope, $http){
+       $scope.init = function() {
+           $scope.listaCategorias = {}
+           
+           $http({
+               method: 'GET',
+               url: '/api/categoria/consultar.php?retornar_imagem=sim'
+           }).then(function(response) {
+               $scope.listaCategorias = response.data;
+           });
+       } 
+    });
